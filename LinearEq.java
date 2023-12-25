36643c3e37897914786f1d6f45d7ad9afe4a8ece
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2023-12-25 19:18:07 +0200

    linearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -1,18 +1,10 @@
-public class LinearEq
-{
-
-public static void main(String[] args) 
-{
-	
-      double 1 = Double.parseDouble(args[0]);
-      double 2 = Double.parseDouble(args[1]);
-      double 3 = Double.parseDouble(args[2]);
-      double x = (3 - 2) / 1;
-      
-System.out.println( 1 + " * " + "x" + " + " + 2 + " = " + 3);
-
-System.out.println( "x = " + x);
-
-        }
+public class LinearEq {
+        public static void main(String[] args) {
+                double a = Double.parseDouble(args[0]);
+                 double b = Double.parseDouble(args[1]); 
+                 double c = Double.parseDouble(args[2]);
+                 double X = (c - b) / a;
+                 System.out.println(a + " * x " + "+ " + b + " = " + c);
+                 System.out.println("x = " + X); 
+         }
 }
-       
