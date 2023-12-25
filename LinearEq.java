2b4189ac85895fb9897c6eb436d3c60b02627a34
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2023-12-25 19:12:01 +0200

    linearEw.java

--- LinearEq.java
+++ LinearEq.java
@@ -1,9 +1,18 @@
-/*
- * Solves linear equations of the form a⋅x + b = c.
- * The program gets a, b, and c as command-line arguments,
- * computes x, and prints the result.
- * Treats the three arguments as well as the computed value as double values
- */
-public class LinearEq {
-	// Put your code here
+public class LinearEq
+{
+
+public static void main(String[] args) 
+{
+	
+      double 1 = Double.parseDouble(args[0]);
+      double 2 = Double.parseDouble(args[1]);
+      double 3 = Double.parseDouble(args[2]);
+      double x = (3 - 2) / 1;
+      
+System.out.println( 1 + " * " + "x" + " + " + 2 + " = " + 3);
+
+System.out.println( "x = " + x);
+
+        }
 }
+       
