52b9954d7c7e8223462aa37b8184460d8c9626ba
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2023-12-25 17:52:53 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,8 @@
 public class AddTwo {
 	public static void main (String [] args) {
-		int 1 = Integer.parseInt(args[0]);
-		int 2 = Integer.parseInt(args[1]);
-		int sum = 1 + 2 ;
-		System.out.println(1+ " + " + 2 + " = "+ sum);
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int sum = a + b ;
+		System.out.println(a+ " + " + b + " = "+ sum);
 	}
 }
