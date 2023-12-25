dc2a2ec29f6d34c277acdc018e44398d95d1d8de
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2023-12-25 18:00:57 +0200

    Coins.java

--- Coins.java
+++ Coins.java
@@ -2,8 +2,14 @@
  * Write a program that gets a quantity of cents as a command-line argument.
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
-public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
-	}
-}
+public class Coins{
+    public static void main(String[] args) {
+    	
+ int coins = Integer.parseInt(args[0]);
+ int quarters = coins/ 25;
+ int remaining = coins%25;
+ 
+  System.out.println("Use " +quarters+ " quarters and " +remaining+ " cents");
+       
+        }
+       }
