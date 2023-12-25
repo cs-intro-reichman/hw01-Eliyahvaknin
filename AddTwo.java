659a9726d2c7132de54a849b864e6d983a8d0055
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2023-12-25 17:42:46 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,14 +1,8 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
 public class AddTwo {
-	public static void main(String[] args) {
-		public static void main
-	}(String[] args) {
-		int numOne = Integer.parseInt(args[0]);
-		int numTwo = Integer.parseInt(args[1]);
-		System.out.println(numOne + " + " + numTwo + " = " + (numOne +
-				numTwo));
-		// Put your code here	
+	public static void main (String [] args) {
+		int 1 = Integer.parseInt(args[0]);
+		int 2 = Integer.parseInt(args[1]);
+		int sum = 1 + 2 ;
+		System.out.println(1+ " + " + 2 + " = "+ sum);
 	}
 }
